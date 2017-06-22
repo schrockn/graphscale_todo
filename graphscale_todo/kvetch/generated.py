3efe44e371a7469bf1f96ea5fe073dec354e154e
Nicholas Schrock <schrockn@gmail.com>
2017-06-22 08:48:02 -0700

    cp

--- graphscale_todo/kvetch/generated.py
+++ graphscale_todo/kvetch/generated.py
@@ -1,11 +1,13 @@
-def generated_objects():
-    return []
+from graphscale.kvetch import define_object
 
+def generated_objects():
+    return [
+        define_object(type_name='TodoUser', type_id=100000),
+        define_object(type_name='TodoItem', type_id=100001),
+    ]
 
 def generated_edges():
     return []
 
-
 def generated_indexes():
     return []
-
--- serve.py
+++ serve.py
@@ -1,7 +1,23 @@
 from graphscale.server import define_graphql_endpoint
 from graphscale_todo.pent import Root
-from graphscale_todo.config import in_mem_context
+from graphscale_todo.config import in_mem_context, single_db_context
 from graphscale_todo.graphql_schema import graphql_schema
+from collections import namedtuple
+
+import pymysql.cursors
+
+ConnectionInfo = namedtuple('ConnectionInfo', 'host user password db charset cursorclass')
+
+
+def get_conn_info():
+    return ConnectionInfo(
+        host='localhost',
+        user='magnus',
+        password='magnus',
+        db='graphscale-todo',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
 
 def serve(context):
@@ -10,4 +26,4 @@ def serve(context):
 
 
 if __name__ == '__main__':
-    serve(in_mem_context())
+    serve(single_db_context(get_conn_info()))
