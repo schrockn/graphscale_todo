9a6f33c7fbab0ada431674df49f1db22cd0c9e6a
Nicholas Schrock <schrockn@gmail.com>
2017-06-27 10:53:54 -0700

    autogen edges

--- graphscale_todo/kvetch/generated.py
+++ graphscale_todo/kvetch/generated.py
@@ -1,5 +1,6 @@
 from graphscale.kvetch import define_object, define_edge, define_index
 
+
 def generated_objects():
     return [
         define_object(type_name='TodoUser', type_id=100000),
@@ -7,9 +8,12 @@ def generated_objects():
         define_object(type_name='TodoItem', type_id=100001),
     ]
 
+
 def generated_edges():
     return [
+        define_edge(edge_name='user_to_list_edge', edge_id=2039430, from_id_attr='owner_id'),
     ]
+
 
 def generated_indexes():
     return []
--- graphscale_todo/kvetch/kvetch_schema.py
+++ graphscale_todo/kvetch/kvetch_schema.py
@@ -6,11 +6,4 @@ def kvetch_schema():
     objects = generated_objects()
     indexes = generated_indexes()
     edges = generated_edges()
-    edges.append(
-        define_edge(
-            edge_name='user_to_list_edge',
-            edge_id=192381923,
-            from_id_attr='owner_id',
-        )
-    )
     return define_schema(objects=objects, indexes=indexes, edges=edges)
