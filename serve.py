5fa8bc54a0cbe7c26e3c2ff779e441bd4873b216
Nicholas Schrock <schrockn@gmail.com>
2017-06-30 17:14:24 -0700

    serve works

--- serve.py
+++ serve.py
@@ -1,22 +1,16 @@
+from graphscale.sql import create_conn_info
 from graphscale.server import run_graphql_endpoint
 from graphscale_todo.pent import Root
-from graphscale_todo.config import in_mem_context, single_db_context
+from graphscale_todo.config import single_db_context
 from graphscale_todo.graphql_schema import graphql_schema
-from collections import namedtuple
-
-import pymysql.cursors
-
-ConnectionInfo = namedtuple('ConnectionInfo', 'host user password db charset cursorclass')
 
 
 def get_conn_info():
-    return ConnectionInfo(
+    return create_conn_info(
         host='localhost',
         user='magnus',
         password='magnus',
         db='graphscale-todo',
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor
     )
 
 
